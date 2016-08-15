a5299c9dc5dc9c059230a6a7968f3c1d826d3bb3
Andrew Karpushin <reven86@gmail.com>
2016-08-15 23:26:42 +0300

    using latest available android ndk toolchain

--- client/source/_android/jni/Application.mk
+++ client/source/_android/jni/Application.mk
@@ -1,8 +1,7 @@
-# SHA1_Library/jni/Application.mk
+# Application.mk
 
 APP_OPTIM := release
 APP_MODULES := dfg-gameplay
 APP_CPPFLAGS += -std=c++11
 APP_STL     := gnustl_static
 APP_ABI     := armeabi-v7a x86
-NDK_TOOLCHAIN_VERSION := 4.8
