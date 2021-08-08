bb130be8983ac1e4a3113bcb11e861929bac3749
Andrew Karpushin <reven86@gmail.com>
2021-08-08 17:04:33 +0300

    hide symbols on android

--- client/source/base/main.cpp
+++ client/source/base/main.cpp
@@ -23,14 +23,14 @@ extern struct android_app* __state;
 extern "C"
 {
 
-void Java_com_dreamfarmgames_util_DFGActivity_receiptReceived(JNIEnv* env, jobject thiz, jstring textObject)
+JNIEXPORT void Java_com_dreamfarmgames_util_DFGActivity_receiptReceived(JNIEnv* env, jobject thiz, jstring textObject)
 {
     const char* text = env->GetStringUTFChars(textObject, NULL);
     static_cast<DfgGame *>(gameplay::Game::getInstance())->onReceiptReceived(text);
     env->ReleaseStringUTFChars(textObject, text);
 }
 
-void Java_com_dreamfarmgames_util_TextViewActivity_textEntered(JNIEnv* env, jobject thiz, jstring textObject)
+JNIEXPORT void Java_com_dreamfarmgames_util_TextViewActivity_textEntered(JNIEnv* env, jobject thiz, jstring textObject)
 {
     const char* text = env->GetStringUTFChars(textObject, NULL);
     static_cast<DfgGame *>(gameplay::Game::getInstance())->onTextViewTextEntered(text);
