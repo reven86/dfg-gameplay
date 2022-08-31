97f370dd21cd10b32cb382724a52d1b336afa127
Andrew Karpushin <reven86@gmail.com>
2022-08-31 20:31:04 +0300

    trying to fix android build error on arm64

--- client/source/_android/jni/Application.mk
+++ client/source/_android/jni/Application.mk
@@ -2,5 +2,5 @@
 
 APP_OPTIM := release
 APP_MODULES := dfg-gameplay
-APP_CPPFLAGS += -fvisibility=hidden -fvisibility-inlines-hidden
+APP_CPPFLAGS += -fvisibility=hidden -fvisibility-inlines-hidden -mno-outline-atomics
 APP_ABI     := armeabi-v7a arm64-v8a x86 x86_64
