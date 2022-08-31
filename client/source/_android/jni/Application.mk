6e57e8398605dcf076ec15e07ef376a2822135d4
Andrew Karpushin <reven86@gmail.com>
2022-08-31 20:38:47 +0300

    trying to fix android build

--- client/source/_android/jni/Application.mk
+++ client/source/_android/jni/Application.mk
@@ -2,5 +2,5 @@
 
 APP_OPTIM := release
 APP_MODULES := dfg-gameplay
-APP_CPPFLAGS += -fvisibility=hidden -fvisibility-inlines-hidden -mno-outline-atomics
+APP_CPPFLAGS += -fvisibility=hidden -fvisibility-inlines-hidden -std=gnu++17
 APP_ABI     := armeabi-v7a arm64-v8a x86 x86_64
