62968faba8dee0a56c159394590782f453f38387
Andrew Karpushin <reven86@gmail.com>
2023-11-05 19:13:42 +0300

    proper prototype declaration

--- client/source/base/services/httprequest_service.cpp
+++ client/source/base/services/httprequest_service.cpp
@@ -11,7 +11,9 @@
 #ifdef __EMSCRIPTEN__
 extern "C"
 {
-    extern int emscripten_async_wget3_data(const char* url, const char* requesttype, const char* data, int dataSize, const char* additionalHeader, void *arg, int free, em_async_wget2_data_onload_func onload, em_async_wget2_data_onerror_func onerror, em_async_wget2_data_onprogress_func onprogress);
+    extern int emscripten_async_wget3_data(const char* url, const char* requesttype, const char* data, int dataSize, const char* additionalHeader, 
+        void *arg, int free, em_async_wget2_data_onload_func onload, em_async_wget2_data_onerror_func onerror, 
+        em_async_wget2_data_onprogress_func onprogress, bool withCredentials);
 }
 #endif
 
