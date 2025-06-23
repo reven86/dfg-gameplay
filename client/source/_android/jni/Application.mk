53e550e3ac4b07db2174728ea36f91e41814691c
Andrew Karpushin <reven86@gmail.com>
2025-06-23 16:24:34 +0300

    enable format on android

--- client/source/_android/jni/Application.mk
+++ client/source/_android/jni/Application.mk
@@ -2,6 +2,6 @@
 
 APP_OPTIM := release
 APP_MODULES := dfg-gameplay
-APP_CPPFLAGS += -std=c++20 -fvisibility=hidden -fvisibility-inlines-hidden
+APP_CPPFLAGS += -std=c++20 -fvisibility=hidden -fvisibility-inlines-hidden -D_LIBCPP_ENABLE_EXPERIMENTAL
 APP_STL      := c++_static
 APP_ABI     := armeabi-v7a arm64-v8a x86 x86_64
