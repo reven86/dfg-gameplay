f53f0c517456adec39593432306a47350c82ab92
Andrew Karpushin <reven86@gmail.com>
2025-06-23 15:31:52 +0300

    fixing builds

--- client/source/_android/jni/Application.mk
+++ client/source/_android/jni/Application.mk
@@ -3,4 +3,5 @@
 APP_OPTIM := release
 APP_MODULES := dfg-gameplay
 APP_CPPFLAGS += -std=c++20 -fvisibility=hidden -fvisibility-inlines-hidden
+APP_STL      := c++_static
 APP_ABI     := armeabi-v7a arm64-v8a x86 x86_64
